7823a48848a055fe82718f72118eedc033479d3f
Maheswari <mahesugumaran@gmail.com>
2025-05-26 21:28:20 +0530

    changes

---
+++ task11/routes/authRoutes.js
@@ -0,0 +1,17 @@
+// routes/authRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+// Route for user registration
+router.post('/register', authController.registerUser);
+
+// Route for user login
+router.post('/login', authController.loginUser);
+
+// Route for getting user info (protected route)
+router.get('/user', authMiddleware.verifyToken, authController.getUserInfo);
+
+module.exports = router;
