95bc09496a72ccc566471b50a388d789acf0c5b5
Maheswari <mahesugumaran@gmail.com>
2025-05-27 16:39:48 +0530

    deleted

--- reciepe-task10/models/recipe.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
- const recipeSchema=new  mongoose.Schema({
-
-    name: String,
-    description: String,
-    price: Number
- })
-
- module.exports=mongoose.model("Recipe",recipeSchema, "recipes")
